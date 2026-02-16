4f91400130a38595dfb4832758326632b4418f9c
ChrishanAloka <raxwotechnology@gmail.com>
2026-02-16 23:43:55 +0530

    Update client.ts

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://barcodegen-4n5g.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
