46768b46a723649f98d2e0af3588da494dbc327a
ChrishanAloka <chrishan2002aloka@gmail.com>
2026-05-07 00:47:20 +0530

    update

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'https://barcodegen-urp1.onrender.com/api';
+export const API_BASE_URL = 'https://barcodegen-4n5g.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
